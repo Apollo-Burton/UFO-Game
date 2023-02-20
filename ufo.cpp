5a77a6210c0349f834192170cd4d8e4f44463b9d
Apollo Burton <124854003+Apollo-Burton@users.noreply.github.com>
2023-02-20 15:56:53 -0800

    Update ufo.cpp

--- ufo.cpp
+++ ufo.cpp
@@ -3,47 +3,47 @@
 
 int main() {
   
-  std::string codeword = "codecademy";
-  std::string answer = "__________";
-  int misses = 0;
-  std::vector<char> incorrect;
-  bool guess = false;
-  char letter;
-  greet();
+	std::string codeword = "codecademy";
+  	std::string answer = "__________";
+  	int misses = 0;
+  	std::vector<char> incorrect;
+  	bool guess = false;
+  	char letter;
+  	greet();
   
-  while (answer != codeword && misses < 7) {
+  	while (answer != codeword && misses < 7) {
     
-    display_status(incorrect, answer);
-    std::cout << "\nPlease enter your guess: ";
-    std::cin >> letter;
+    	display_status(incorrect, answer);
+    	std::cout << "\nPlease enter your guess: ";
+    	std::cin >> letter;
     
-    for (int i = 0; i < codeword.length(); i++) {
+    	for (int i = 0; i < codeword.length(); i++) {
 
-      if (letter == codeword[i]) {
+      		if (letter == codeword[i]) {
 
-        answer[i] = letter;
-        guess = true;
+        	answer[i] = letter;
+        	guess = true;
 
-      }
+      		}
     
-    }
+    	}	
 
-    if (guess) {
+    	if (guess) {
 
-      std::cout << "Correct!\n";
+      		std::cout << "Correct!\n";
 
-    } else {
+    	} else {
 
-      std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
-      incorrect.push_back(letter);
-      misses++;
+      		std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
+      		incorrect.push_back(letter);
+      		misses++;
 
-    }
-    display_misses(misses);
-    guess = false;
+    	}
+    	display_misses(misses);
+    	guess = false;
     
-  }
+  	}
 
-  end_game(answer, codeword);
+  	end_game(answer, codeword);
 
 }
